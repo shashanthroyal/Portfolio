4f360f2b67dabf9cb0632614e2b01ac9fb7c2570
shashanthroyal <shashanth.chandu@gmail.com>
2025-05-12 15:57:11 +0530

    second commit

--- script.js
+++ script.js
@@ -1,30 +1,12 @@
-let ResumeButton = Document.getElementById("resume-button");
 
-if (ResumeButton) {
-    ResumeButton.addEventListener('click', function() {
-        const a = document.createElement('a');
-        a.href = "./Resume.pdf";
-        a.target = '_blank';
-        a.rel = 'noopener noreferrer';
-        a.click();
-    });
-}
+const toggleButton = document.querySelector('.toggle-button');
+const navLinks = document.querySelector('.nav-links');
 
-const form = document.getElementById('form');
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault(); // Prevent the default form submission
-
-  const formData = new FormData(form);
-  const data = {};
-  formData.forEach((value, key) => {
-    data[key] = value;
-  });
-
-  console.log('Form Data:', data);
-
-  // You would typically send this 'data' to the server here
+toggleButton.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
 });
+
+
 
 
 
